1eb6c8b519497582ad01d1ec687860c6b14ef3c3
jonathan gerber <jlgerber@gmail.com>
2019-03-26 08:49:02 -0700

    fixes pylint issues in dictmerge.py

--- logging_eg/dictmerge.py
+++ logging_eg/dictmerge.py
@@ -2,32 +2,35 @@
 More or less a direct "homage" to stackoverflow code, with the
 exception of not merging lists, and raising a KeyError instead of an Exception.
 """
-def merge(a, b, path=None, update=True):
+def merge(dict_a, dict_b, path=None, update=True):
     """
-    Given two dicts, a and b, merge b into a.
-    code from: "http://stackoverflow.com/questions/7204805/python-dictionaries-of-dictionaries-merge"
+    Given two dicts, dict_a and dict_b, merge dict_b into dict_a.
+    code from:
+    "http://stackoverflow.com/questions/7204805/python-dictionaries-of-dictionaries-merge"
 
-    :param a: The dictionary we wish to update
-    :parm b: The dictionary we wish to update `a` with
-    :returns: a
+    :param dict_a: The dictionary we wish to update
+    :parm dict_b: The dictionary we wish to update `dict_a` with
+    :returns: dict_a
     :raises: KeyError
     """
-    if path is None: path = []
-    for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
+    if path is None:
+        path = []
+    for key in dict_b:
+        if key in dict_a:
+            if isinstance(dict_a[key], dict) and isinstance(dict_b[key], dict):
+                merge(dict_a[key], dict_b[key], path + [str(key)])
+            elif dict_a[key] == dict_b[key]:
                 pass # same leaf value
-            elif isinstance(a[key], list) and isinstance(b[key], list):
+            elif isinstance(dict_a[key], list) and isinstance(dict_b[key], list):
                 # we replace lists instead of merging their contents.
-                #for idx, val in enumerate(b[key]):
-                #    a[key][idx] = merge(a[key][idx], b[key][idx], path + [str(key), str(idx)], update=update)
-                a[key] = b[key]
+                #for idx, val in enumerate(dict_b[key]):
+                #    dict_a[key][idx] = merge(dict_a[key][idx], dict_b[key][idx], \
+                # path + [str(key), str(idx)], update=update)
+                dict_a[key] = dict_b[key]
             elif update:
-                a[key] = b[key]
+                dict_a[key] = dict_b[key]
             else:
                 raise KeyError('Conflict at %s' % '.'.join(path + [str(key)]))
         else:
-            a[key] = b[key]
-    return a
+            dict_a[key] = dict_b[key]
+    return dict_a
